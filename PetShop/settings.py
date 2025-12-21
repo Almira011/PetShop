fe6293a98e2f29665e9711b34447558a6d009640
Almira011 <almirushautepova@gmail.com>
2025-12-21 18:31:23 +0500

    css224455

--- PetShop/settings.py
+++ PetShop/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Optional if you have a global static folder
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # or os.path.join(BASE_DIR, "static") for older versions
+]
+
+# For deployment
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
