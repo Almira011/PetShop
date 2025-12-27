fb90aaaa94eefef11432e4681eddbe3429ce1525
Almira011 <almirushautepova@gmail.com>
2025-12-27 18:13:45 +0500

    database11233334555

--- PetShop/settings.py
+++ PetShop/settings.py
@@ -92,7 +92,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(
-         os.getenv("DATABASE_URL")
+        "postgresql://postgres_d6g9_user:o6Oi4S8qOCEqeGw0z7faGTIMUNXwubgd@dpg-d4q2csruibrs73dedqcg-a/postgres_d6g9"
     )
 }
 # Password validation
